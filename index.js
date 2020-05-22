3bd4df80f76639c43951d447fda69787a529ad3a
Roman Silen <romansilen@gmail.com>
2020-05-22 16:41:15 -0400

    Added side navigation bar

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById('date').innerHTML = new Date().toDateString();
+
+// set width of sidenav to 250px and push main left 250px
+function openNav() {
+	document.getElementById("sideNav").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+}
+
+// set width of sidenav to 0px and push main right 250px
+function closeNav() {
+	document.getElementById("sideNav").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+}
